a8b497c2187f9090dce92a91c54c774f5a503219
Maneesh Sahu <maneesh@sahu.haus>
2020-04-15 13:58:31 -0700

    Simplified route for delete

--- domains/delete.js
+++ domains/delete.js
@@ -7,25 +7,52 @@ const { cors, httpErrorHandler } = require("middy/middlewares");
 const removeDomain = async (event) => {
   console.log("domain", event.pathParameters);
 
-  let { name, idp } = event.pathParameters;
+  let { name } = event.pathParameters;
 
-  const params = {
-    TableName: process.env.DYNAMODB_TABLE,
-    Key: {
-      name,
-      idp,
-    },
-    ConditionExpression: "attribute_exists(#name)",
-    ExpressionAttributeNames: {
-      "#name": "name",
-    },
-    ReturnValues: "ALL_OLD",
-  };
-
-  console.log("Delete Item", JSON.stringify(params));
-
-  // delete the domain from the database
   try {
+    const queryParams = {
+      TableName: process.env.DYNAMODB_TABLE,
+      IndexName: "domains-" + process.env.DYNAMODB_TABLE,
+      KeyConditionExpression: "#name = :value",
+      ExpressionAttributeNames: {
+        "#name": "name", // name is a reserved word, should have just used domain :P
+      },
+      ExpressionAttributeValues: {
+        ":value": name,
+      },
+      FilterExpression: "attribute_exists(idp)",
+      ProjectionExpression: "idp",
+    };
+
+    // fetch all domains from the database that match an idp
+    let result = await dynamodb.query(queryParams).promise();
+
+    console.log(JSON.stringify(result.Items));
+
+    if (result.Items.length != 1) {
+      return {
+        statusCode: 404,
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          error: `No entry for domain: '${name}'`,
+        }),
+      };
+    }
+
+    let idp = result.Items[0].idp;
+
+    const params = {
+      TableName: process.env.DYNAMODB_TABLE,
+      Key: {
+        name,
+        idp,
+      },
+      ReturnValues: "ALL_OLD",
+    };
+
+    console.log("Delete Item", JSON.stringify(params));
+
+    // delete the domain from the database
     let res = await dynamodb.delete(params).promise();
     console.log("Dynamo db", JSON.stringify(res));
     return {
